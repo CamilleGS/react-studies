2aa47b379a214b6b67cfcbc6051adb221b5f93ce
Aluno <example@example.com>
2025-03-18 20:56:48 -0300

    added an api

--- src/App.js
+++ src/App.js
@@ -1,57 +1,68 @@
-import React, {useState} from 'react'
-import People from './assets/people.png'
-import Arrow from './assets/arrowright.png'
-import Trash from './assets/trash.png'
-import { Container, H1, Image, ContainerItems, InputLabel, Input, Button, User } from './styles'
+import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import People from "./assets/people.png";
+import Arrow from "./assets/arrowright.png";
+import Trash from "./assets/trash.png";
+import { Container, H1, Image, ContainerItems, InputLabel, Input, Button, User } from "./styles";
 
 const App = () => {
+  const [users, setUsers] = useState([]); //react hooks => ferramentas auxiliares
+  const inputName = useRef();
+  const inputAge = useRef();
+  //estado no react é imutavel
 
-  //const users = []
+  async function addNewUser() {
+    /* const { data: newUser } = await axios.post("http://localhost:3001/users", {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+    });
 
-  const [users, setUsers] = useState([]) //react hooks => ferramentas auxiliares
+    console.log(newUser);
+    setUsers([...users, newUser]); */
 
- //estado no react é imutavel
-
-  function addNewUser(){
-   setUsers([{ id: Math.random(), name: 'Rodolfo', age: 28 }])
-
-   // console.log(users)
-  
+    const { data: newUsers } = await axios.get("http://localhost:3001/users");
+    setUsers(newUsers);
   }
 
-  
+  useEffect(() => {
+    //inicia assim que a pagina carregar
+    console.log("abcde");
+  }, [users]);
+  function deleteUSer(userId) {
+    console.log(userId);
+    const newUsers = users.filter((user) => user.id !== userId);
+    setUsers(newUsers);
+  }
 
   return (
     <Container>
-      <Image alt='logo-img' src={People} />
+      <Image alt="logo-img" src={People} />
       <ContainerItems>
         <H1>Olá</H1>
         <InputLabel>Nome</InputLabel>
-        <Input placeholder='Nome' />
+        <Input ref={inputName} placeholder="Nome" />
 
         <InputLabel>Idade</InputLabel>
-        <Input placeholder='Idade' />
-       {/*  não pode por parenteses no add newUser */}
-        <Button onClick={addNewUser}>Cadastrar <img src={Arrow} alt='arrow' /></Button>
+        <Input ref={inputAge} placeholder="Idade" />
+        {/*  não pode por parenteses no add newUser */}
+        <Button onClick={addNewUser}>
+          Cadastrar <img src={Arrow} alt="arrow" />
+        </Button>
 
-
-        <ul>{
-          users.map(user => (
+        <ul>
+          {users.map((user) => (
             <User key={user.id}>
-             <p>{user.name} </p>  <p>{user.age}</p> 
-              <button> <img src={Trash} alt='trash'/></button>
+              <p>{user.name} </p> <p>{user.age}</p>
+              <button onClick={() => deleteUSer(user.id)}>
+                {" "}
+                <img src={Trash} alt="trash" />
+              </button>
             </User>
-          ))
-        }
+          ))}
         </ul>
       </ContainerItems>
-
-
-
     </Container>
-  )
+  );
+};
 
-
-}
-
-export default App
+export default App;
